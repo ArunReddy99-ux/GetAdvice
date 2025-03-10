0213b35d3b691a5e25e3bf2e16bd68e8cc1bbf38
ArunReddy99-ux <arunreddyveerelly2003@gmail.com>
2025-03-10 22:19:16 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [Advice, setAdvice] = useState("");
+  const [count, setcount] = useState(0);
+  async function getAdvice() {
+    const ans = await fetch("https://api.adviceslip.com/advice");
+    const res = await ans.json();
+    setAdvice(res.slip.advice);
+    setcount((c) => c + 1);
+  }
+  useEffect(function () {
+    getAdvice();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{Advice}</h1>
+      <button onClick={getAdvice}>Get Advice</button>
+      <Message count={count} />
     </div>
   );
 }
-
-export default App;
+function Message(props) {
+  return (
+    <p>
+      You have <strong>{props.count}</strong> pieces of Advice
+    </p>
+  );
+}
